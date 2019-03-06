260b1c44dbfa419c02237d0c923978e60ce7ce0a
vatsal <vatsalsoni337@gmail.com>
2019-03-06 16:50:02 +0530

    first commit

---
+++ toast-library/src/main/java/com/example/toast_library/ToastMessage.java
@@ -0,0 +1,12 @@
+package com.example.toast_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+
+    public void s(Context context,String message){
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+
+}
