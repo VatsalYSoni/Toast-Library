f09e32ef9e027f882fa98fe505fbe9969a376c73
vatsal <vatsalsoni337@gmail.com>
2019-03-06 17:24:49 +0530

    change

--- toast-library/src/main/java/com/example/toast_library/ToastMessage.java
+++ toast-library/src/main/java/com/example/toast_library/ToastMessage.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class ToastMessage {
 
-    public void s(Context context,String message){
+    public static void s(Context context,String message){
         Toast.makeText(context,message,Toast.LENGTH_LONG).show();
     }
 
